8a1a5dee2ecd493c61fb869ea40984038b9610ff
tutuxin <it_ltxin@163.com>
2017-07-27 10:20:05 +0800

    server

--- cmdb/models/Server.py
+++ cmdb/models/Server.py
@@ -9,7 +9,7 @@ from common.models import BaseModel
 class Server(BaseModel):
     host_group = models.ForeignKey(HostGroup, verbose_name='主机组', blank=True, null=True)
     host = models.CharField(max_length=255, blank=True, null=True, verbose_name="主机名")
-    mem_total = models.IntegerField(blank=True, null=True, verbose_name="内存大小")
+    mem_total = models.ForeignKey(Host, verbose_name='mem_total', blank=True, null=True)
     num_cpus = models.IntegerField(blank=True, null=True, verbose_name="CPU数量")
     idc = models.ForeignKey(IDC, verbose_name='IDC', blank=True, null=True)
     server = models.CharField(max_length=255, blank=True, null=True, verbose_name="服务")
