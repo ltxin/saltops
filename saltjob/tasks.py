86365c9fa96e0e8d44a5b4457c057db21adea7e6
tutuxin <it_ltxin@163.com>
2017-08-01 16:00:00 +0800

    server

--- saltjob/tasks.py
+++ saltjob/tasks.py
@@ -695,11 +695,11 @@ def scanNginxJob():
     logger.info('扫描Nginx')
     upList = []
     try:
-        manageInstance = salt_api_token({'fun': 'dis_nginx.run','tgt': 'nginx','expr_form':'nodegroups'},
+        manageInstance = salt_api_token({'fun': 'dis_nginx.run','tgt': 't1.e-nci.com','expr_form':'list'},
                                         SALT_REST_URL, {'X-Auth-Token': token_id()})
         statusResult = manageInstance.CmdRun()
         result = statusResult['return'][0]
         print(result)
     except Exception as e:
-        logger.info("发现的server" % e)
+        logger.info("发现的nginxs有问题" % e)
     print(upList)
