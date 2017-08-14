b05b324cfab0cae718b3c3baaa6de3b69db1a3f2
tutuxin <it_ltxin@163.com>
2017-08-14 16:56:35 +0800

    saltops

--- deploy_manager/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'deploy_manager.apps.DeployManagerConfig'
--- deploy_manager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DeployManagerConfig(AppConfig):
-    name = 'deploy_manager'
-    verbose_name = '发布管理'
